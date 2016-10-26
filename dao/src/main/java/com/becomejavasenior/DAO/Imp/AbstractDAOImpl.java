a6801b974111a5a66d406e5bd01bacb01c1a88c1
Viktor <viktor_mag@mail.ru>
2016-10-26 23:46:36 +0300

    Реализация Абстрактного ДАО класса

---
+++ dao/src/main/java/com/becomejavasenior/DAO/Imp/AbstractDAOImpl.java
@@ -0,0 +1,183 @@
+package com.becomejavasenior.DAO.Imp;
+
+import com.becomejavasenior.DAO.AbstractDAO;
+import com.becomejavasenior.DataBaseUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractDAOImpl<T> implements AbstractDAO<T> {
+
+
+    @Override
+    public T create(T entity) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet;
+        try {
+            connection = DataBaseUtil.getConnection();
+            String query = getCreateQuery();
+            preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            createStatement(preparedStatement, entity);
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                Integer generatedId = resultSet.getInt(1);
+                return getById(generatedId);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) try {
+                preparedStatement.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    abstract void createStatement(PreparedStatement preparedStatement, T entity);
+
+    @Override
+    public T update(T entity) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DataBaseUtil.getConnection();
+            String query = getUpdateQuery();
+            preparedStatement = connection.prepareStatement(query);
+            updateStatement(preparedStatement, entity);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) try {
+                preparedStatement.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    abstract void updateStatement(PreparedStatement preparedStatement, T entity);
+
+    @Override
+    public void delete(Integer id) {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DataBaseUtil.getConnection();
+            String query = getDeleteQuery();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) try {
+                preparedStatement.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public T getById(Integer id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet;
+        try {
+            connection = DataBaseUtil.getConnection();
+            String query = getByIdQuery();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return getEntity(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) try {
+                preparedStatement.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    abstract T getEntity(ResultSet resultSet);
+
+    @Override
+    public List<T> getAll() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet;
+        List<T> listEntity = new ArrayList<>();
+        try {
+            connection = DataBaseUtil.getConnection();
+            String query = getAllQuery();
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                listEntity.add(getEntity(resultSet));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) try {
+                preparedStatement.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException logOrIgnore) {
+                logOrIgnore.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    abstract String getCreateQuery();
+
+    abstract String getUpdateQuery();
+
+    abstract String getDeleteQuery();
+
+    abstract String getByIdQuery();
+
+    abstract String getAllQuery();
+
+
+}
