cb99cc6c099918e47fb804abd79f6b6addd28685
krummerd <krummerdmitriy@yahoo.com>
2016-11-30 10:30:37 +0200

    asd

--- dao/src/main/java/com/becomejavasenior/DataBaseUtil.java
+++ dao/src/main/java/com/becomejavasenior/DataBaseUtil.java
@@ -81,8 +81,12 @@ public class DataBaseUtil {
         } catch (IOException | ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }*/
+        try {
+        Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
 
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/crm_pallas","postgres", "root");
+        }
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/crm_pallas","postgres", "1111");
     }
     public static String getQuery(String name) {
         return name;
