69929e48bc9a39276336932d305a729de95043cc
Artem Zhukov <atem085@gmail.com>
2016-12-01 19:54:04 +0200

    servlet

---
+++ dao/src/main/java/com/becomejavasenior/DAO/DaoException.java
@@ -0,0 +1,31 @@
+package com.becomejavasenior.DAO;
+
+import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+
+
+public class DaoException extends Exception {
+
+    public static Logger log = Logger.getLogger(DaoException.class);
+
+    public DaoException() {
+
+        super();
+
+    }
+
+    public DaoException(String errorMsg) {
+
+        super(errorMsg);
+        log.error(errorMsg);
+
+    }
+
+    public DaoException(String errorMsg, SQLException exception) {
+
+        super(errorMsg);
+        log.error(errorMsg, exception);
+
+    }
+}
